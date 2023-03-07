c44481b27ae7a48f372062fc3a230a81a4e2ced6
NEDJAR Amine p2105787 <amine.nedjar@etu.univ-lyon1.fr>
2023-03-07 12:29:05 +0100

    Ajout Personnage.cpp

---
+++ src/Personnage.h
@@ -0,0 +1,38 @@
+#ifndef _PERSONNAGE_H
+#define _PERSONNAGE_H
+
+#include "Projectile.h"
+#include "Terrain.h"
+#include "Vecteur.h"
+
+class Personnage {
+private :
+	Vecteur pos /* @brief les coordonnées x,y de notre personnage */
+	int pv; /* @brief les points de vie de notre personnage */
+	Projectile proj; /* @brief le projectile qu'il va tirer sur les monstres */
+	
+public :
+	Personnage(); /* @brief constructeur du personnage */
+	~Personnage(); /* @brief destructeur */
+	void depGauche(const Terrain &t);
+	/* @brief procédure pour se déplacer à gauche 
+	@param le terrain */
+	void depDroite(const Terrain &t);
+	/* @brief procédure pour se déplacer à droite 
+	@param le terrain */
+	void depHaut(const Terrain &t);
+	/* @brief procédure pour se déplacer en haut 
+	@param le terrain */
+	void depBas(const Terrain &t);
+	/* @brief procédure pour se déplacer en bas 
+	@param le terrain */
+	int getPV() const;
+	/* @brief renvoie les points de vie du personnage */
+	Vecteur getPos()const;
+	/* @brief renvoie la position du personnage */
+	bool enVie() const;
+	/* @brief renvoie true si le personnage est en vie et false si le personnage est mort */
+}
+
+
+#endif
