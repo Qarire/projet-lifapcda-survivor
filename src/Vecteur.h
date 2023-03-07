c3d8b66b6c19bae75d332a1a2c7119f960d48811
DIA ABDOU-KARIM p2006809 <p2006809@b710l0106.univ-lyon1.fr>
2023-03-07 11:19:48 +0100

    Modification vecteur.h

---
+++ src/Vecteur.h
@@ -0,0 +1,33 @@
+#ifndef _VECTEUR_H
+#define _VECTEUR_H
+
+
+class Vecteur {
+private :
+    float x,y;
+
+public :
+
+    float getX(); const
+    //Recupere la coordonnée X
+
+    float getY(); const
+    //Recupere la coordonnée Y
+
+    Vecteur make_vec(float x, float y);
+    //Fabrique un vecteur de coordonnée x,y
+
+    Vecteur operator+(Vecteur a, float lambda);
+    //operation + avec un Vecteur et un réel
+
+    Vecteur operator-(Vecteur a, float lambda);
+    //operation - avec un Vecteur et un réel
+
+    Vecteur operator*(Vecteur a, float lambda);
+    //operation * avec un Vecteur et un réel
+
+    Vecteur operator/(Vecteur a, float lambda);
+    //operation / avec un Vecteyr et un réel
+
+
+}
