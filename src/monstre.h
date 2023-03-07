a9fd4214820e05d62f804a1ede723d3ea81862bf
NEDJAR Amine p2105787 <amine.nedjar@etu.univ-lyon1.fr>
2023-03-07 10:37:02 +0100

    Modefications monstre.h

--- src/monstre.h
+++ src/monstre.h
@@ -1,1 +1,29 @@
-a
+#ifndef _MONSTRE
+#define _MONSTRE
+
+#include<iostream>
+using namespace std;
+
+
+
+class monstre 
+{
+	private:
+	/**
+   * @brief x,y : les composantes du monstre, pv: ses pint de vie
+   * */
+	int x,y;
+	int pv;
+	public :
+	 /**
+     @brief Constructeur par défaut de la classe: initialise la position de monstre, ses points de vie
+     **/
+     monstre();
+     /**
+     @brief destructeur par défaut de la classe: initialise la position de monstre, ses points de vie
+     **/
+     
+		 
+};
+
+#endif
