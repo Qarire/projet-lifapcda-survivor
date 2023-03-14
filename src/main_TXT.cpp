d9f9df997568f5486bdbefeb0a784b312597b7c8
Amine p2105787 <amine.nedjar@eut.univ-lyon1.fr>
2023-03-14 10:24:55 +0100

    Ajout mainTXT

---
+++ src/main_TXT.cpp
@@ -0,0 +1,9 @@
+//sudo apt install python3 valgrind doxygen libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev libsdl2-mixer-dev imagemagick
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	return 0;
+}
