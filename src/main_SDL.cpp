7834ea306b7cd56590893547b92431299116271c
Amine p2105787 <amine.nedjar@eut.univ-lyon1.fr>
2023-04-04 08:46:09 +0200

    main

--- src/main_SDL.cpp
+++ src/main_SDL.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
+#include "winSdl.h"
 using namespace std;
 
-int main()
-{
+int main (int argc, char** argv) {
+	SDLSimple j;
+	j.sdlBoucle();
+	cout<<" *** FIN PROG *** "<<endl;
 	return 0;
 }
+
