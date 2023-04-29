b2cd4aeeec0b78f2bef1d0a12fa1ffab81313bf5
NEDJAR Amine p2105787 <amine.nedjar@etu.univ-lyon1.fr>
2023-04-29 11:37:21 +0200

    Optémisation déplacements monstres + corrextion beugs mineurs

--- src/Monstre.cpp
+++ src/Monstre.cpp
@@ -4,7 +4,6 @@
 #include "Monstre.h"
 using namespace std;
 
-
 Monstre::Monstre(){
     m_pos = Vecteur();
 	m_pv = 50;
@@ -77,7 +76,25 @@ void Monstre::depBas(const Terrain &t)
 	}
 }
 
-void Monstre::poursuitePerso(const Terrain &t, const Personnage &p){
+
+// poursuitePerso : vrsion 1
+/* void Monstre::poursuitePerso(const Terrain &t, const Personnage &p){  
+    if (t.positionValide(m_pos.getX(),m_pos.getY()))
+	{
+			  if (p.getPos().getX()>m_pos.getX()) depDroite(t);
+
+			  if (p.getPos().getX()<m_pos.getX()) depGauche(t);
+
+			  if (p.getPos().getY()>m_pos.getY()) depBas(t);
+
+			  if (p.getPos().getY()<m_pos.getY()) depHaut(t);
+	}
+}
+*/
+
+
+// poursuitePerso : vrsion 2
+/* void Monstre::poursuitePerso(const Terrain &t, const Personnage &p){  
     if (t.positionValide(m_pos.getX(),m_pos.getY()))
 	{
 		if ((p.getPos().getX()<m_pos.getX()) && (p.getPos().getY()<m_pos.getY()))
@@ -133,21 +150,61 @@ void Monstre::poursuitePerso(const Terrain &t, const Personnage &p){
 		if ((p.getPos().getX()==m_pos.getX()) && (p.getPos().getY()>m_pos.getY())) depBas(t);
 		if ((p.getPos().getX()<m_pos.getX()) && (p.getPos().getY()==m_pos.getY())) depGauche(t);
 		if ((p.getPos().getX()>m_pos.getX()) && (p.getPos().getY()==m_pos.getY())) depDroite(t);
-		
-		// PERSO COORD : p.getPos().getX(),p.getPos().getY()
-		// MONSTRE COORD : m_pos.getX(), m_pos.getY()
-			
-		//	if (p.getPos().getX()>m_pos.getX()) depDroite(t);
-
-		//   if (p.getPos().getX()<m_pos.getX()) depGauche(t);
-
-		//    if (p.getPos().getY()>m_pos.getY()) depBas(t);
-
-		//    if (p.getPos().getY()<m_pos.getY()) depHaut(t);
     }
+}*/
+
+
+// poursuitePerso : version 3
+
+void Monstre::poursuitePerso(const Terrain& t, const Personnage& p) {  
+	if (!t.positionValide(m_pos.getX(), m_pos.getY())) { 
+	return;
+	}
+	// Obtenir la position du joueur et du monstre
+	const int mX = m_pos.getX();
+	const int mY = m_pos.getY();
+	const int pX = p.getPos().getX();
+	const int pY = p.getPos().getY();
+
+	// Si le monstre et le joueur sont sur la même colonne ou ligne, se déplacer dans la direction correspondante
+	if (pX == mX) {
+		if (pY < mY) {
+			depBas(t);  // Inverser la direction
+		} else if (pY > mY) {
+			depHaut(t);  // Inverser la direction
+		}
+		return;
+	}
+	if (pY == mY) {
+		if (pX < mX) {
+			depDroite(t);  // Inverser la direction
+		} else if (pX > mX) {
+			depGauche(t);  // Inverser la direction
+		}
+		return;
+	}
+
+	// Calculer la distance entre le joueur et le monstre dans chaque direction
+	const int dxG = mX - pX;
+	const int dxD = pX - mX;
+	const int dyH = mY - pY;
+	const int dyB = pY - mY;
+
+	// Déterminer la direction de déplacement avec la distance la plus courte
+	if (dxG > dyH && dxG > dyB) {  // Inverser les conditions pour rapprocher le monstre
+		depGauche(t);
+	} else if (dxD > dyH && dxD > dyB) {
+		depDroite(t);
+	} else if (dyH > dxG && dyH > dxD) {
+		depHaut(t);
+	} else {
+		depBas(t);
+	}
 }
 
-void Monstre::deplacementNaturel(const Terrain &t) {
+//Déplacer un monstre aléatoirement dans une direction
+
+void Monstre::deplacementNaturel(const Terrain &t) {  
     // définition des vecteurs de direction possibles
     vector<Vecteur> directions = { {1,0}, {-1,0}, {0,1}, {0,-1}, {1,1}, {-1,-1}, {1,-1}, {-1,1} };
     int n_directions = directions.size();
